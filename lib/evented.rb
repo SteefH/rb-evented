3170ece6e9f692c7b0c447d92f4bbc47c03bf75e
Stefan van der Haven <stefan@steeffie.net>
2018-07-17 20:47:19 +0200

    Event journal stuff

--- lib/evented.rb
+++ lib/evented.rb
@@ -26,30 +26,57 @@ module Evented
     end
 
     module InstanceMethods # rubocop:disable Style/Documentation
-      attr_reader :entity_id
-
-      def emit(event_type, **payload)
-        handler = @event_handlers[event_type]
-        instance_exec(payload, &handler) if handler
-        @entity_version += 1
-        Event.new(self.class, @entity_id, @entity_version, event_type, payload)
-      end
+      attr_reader :entity_id, :entity_version
+      
     end
 
     module ClassMethods # rubocop:disable Style/Documentation
+
       def on(event_type, &block)
-        (@event_handlers ||= {})[event_type] = block
+        event_handlers[event_type] = block
       end
 
-      def load(id)
+      def load(id, event_journal, snapshot_store = nil)
+        entity = create_new_entity(id, event_journal)
+        entity = load_snapshot(entity, snapshot_store)
+        event_journal.load_events(entity.class, id, entity.entity_version + 1) do |event|
+          handler = event_handlers[event.event_type]
+          entity.instance_exec event.payload, &handler
+          entity.instance_exec { @entity_version = event.entity_version }
+        end
+        entity
+      end
+
+      private
+
+      def event_handlers
+        @event_handlers ||= {}
+      end
+
+      def create_new_entity(id, event_journal)
         i = new
-        event_handlers = @event_handlers
-        i.instance_eval do
-          @entity_id = id
-          @entity_version = 0
-          @event_handlers = event_handlers
+        handlers = event_handlers
+        i.instance_eval { @entity_id = id; @entity_version = 0; }
+        i.define_singleton_method(:emit) do |event_type, payload = nil|
+          handler = handlers[event_type]
+          instance_exec(payload, &handler) if handler
+          @entity_version += 1
+          evt = Event.new(self.class, @entity_id, @entity_version, event_type, payload)
+          event_journal.store_event evt
+          evt
+        end
+        class << i
+          private :emit
         end
         i
+      end
+
+      def load_snapshot(entity, snapshot_store)
+        return entity unless (entity.respond_to? :apply_snapshot) && !snapshot_store.nil?
+        snapshot = snapshot_store.load_snapshot entity.class, entity.entity_id
+        entity.apply_snapshot snapshot
+        entity.instance_eval { @entity_version = snapshot.version }
+        entity
       end
     end
   end
--- spec/evented_spec.rb
+++ spec/evented_spec.rb
@@ -38,27 +38,51 @@ class BreadBox
     @number_of_slices = 0
   end
 
-  def eat(number_of_slices = 1)
-    number_of_slices_that_can_be_eaten = [number_of_slices, @number_of_slices].min
-    emit(:eaten, number_of_slices_that_can_be_eaten) if number_of_slices_that_can_be_eaten > 0
+  def take(number_of_slices = 1)
+    allowed_number_of_slices = [number_of_slices, @number_of_slices].min
+    emit(:slices_taken, allowed_number_of_slices) if allowed_number_of_slices > 0
   end
 
   def refill(number_of_slices)
-    emit(:refill, number_of_slices)
+    emit(:refilled, number_of_slices)
   end
 
-  on(:eaten) do |number_of_slices|
+  on(:slices_taken) do |number_of_slices|
     @number_of_slices -= number_of_slices
   end
 
-  on(:refill) do |number_of_slices|
+  on(:refilled) do |number_of_slices|
     @number_of_slices += number_of_slices
+  end
+end
+
+class InMemoryJournal
+  def initialize
+    @events = Hash.new { |by_class,klass|
+        by_class[klass] = Hash.new { |by_id,id|
+          by_id[id] = [] 
+        } 
+    }
+  end
+
+  def load_events(klass, id, start_version)
+    @events[klass][id].each do |evt|
+      puts "LOADING #{evt.to_s}"
+      yield evt if evt.entity_version >= start_version
+    end
+  end
+
+  def store_event(evt)
+    klass = evt.entity_class
+    id = evt.entity_id
+    @events[klass][id] << evt
   end
 end
 
 RSpec.describe Evented do
   it 'creates events' do
-    c = CoffeeMug.load(1)
+    journal = InMemoryJournal.new
+    c = CoffeeMug.load(1, journal)
     expect(c.state).to eq(:empty)
     # expect(c.entity_version).to eq(0)
     expect(c.drink).to eq(nil)
@@ -72,8 +96,12 @@ RSpec.describe Evented do
     e = c.refill
     expect(c.state).to eq(:full)
     expect(e.event_type).to eq(:refilled)
-    bb = BreadBox.load(1)
-
+    c2 = CoffeeMug.load(1, journal)
+    expect(c2.state).to eq(c.state)
+    expect(c2.entity_version).to eq(c.entity_version)
+    bb = BreadBox.load(1, journal)
+    bb.take
+    bb.refill 100
     # expect(e.at).to eq('now')
   end
 end
